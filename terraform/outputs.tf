73f0cd14ebe5c5e9f1f934eecc2a1bef66efbeb5
Mbala12 <isaacnkongolo64@gmail.com>
2025-09-06 21:44:10 -0400

    update of some files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
-# output "ec1_public_ip" {
-#   value = aws_instance.ubuntu_ec1.public_ip
-# }
+output "ec1_public_ip" {
+  value = aws_instance.ubuntu_ec1.public_ip
+}
 
-# output "ec2_public_ip" {
-#   value = aws_instance.ubuntu_ec2.public_ip
-# }
+output "ec2_public_ip" {
+  value = aws_instance.ubuntu_ec2.public_ip
+}
